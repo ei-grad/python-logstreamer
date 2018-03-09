b9fba168d6c8d3f21920d634a73a31da686b367f
Andrew Grigorev <andrew@ei-grad.ru>
2018-03-09 14:22:36 +0300

    Initial commit

---
+++ logstreamer/__init__.py
@@ -0,0 +1,13 @@
+class Watcher():
+    def __init__(self, path, target, splitter=None):
+        """
+        Create new Watcher.
+
+        Watcher setups inotify watches on file set specified by `path` and tails
+        """
+        pass
+
+    def run(self):
+        """
+        Run watcher.
+        """
